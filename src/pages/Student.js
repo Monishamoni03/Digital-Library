24415a5ceb34b7643dd59f4442d0aaf3380d1a85
Monisha G <83082244+Monishamoni03@users.noreply.github.com>
2022-07-20 09:13:33 +0530

    19/07/2022

--- src/pages/Student.js
+++ src/pages/Student.js
@@ -1,41 +1,68 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import '../assets/css/Login.css'; 
+import React, { Component, useState } from 'react';
+import '../assets/css/Login.css';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../redux/action';
+import { useNavigate } from 'react-router-dom';
 import NavBar from './NavBar';
+import WelcomePage from './WelcomePage';
 
 class Student extends Component {
   render() {
+  const [state, setState] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [error, setError] = useState("");
+
+  // let history = useHistory();
+  const navigate = useNavigate();
+  let dispatch = useDispatch();
+
+  const { email, password } = state;
+
+  const handleInputChange = (e) => {
+    let { name, value } = e.target;
+    setState({...state, [name]: value});
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!email || !password) {
+      setError("*Please fill all the input fields*");
+    } else {
+      dispatch(addUser(state));
+      // history.push("/login"); 
+      navigate('/welcome');
+      // setError("");
+    }
+  };
 
   return (
-  <div>
+    <div>
       <NavBar />
-    <div className = 'login-student'>
+      <div className = 'login-student'>
       <div className = 'login-container'>
         <h3>Log in to digital library</h3>
-        <form className = 'login-form '>
+        {error && <h6 style = {{color: "red" }}>{error}</h6>}
+        <form className = 'login-form' onSubmit = {handleSubmit}>
           <center>
-            <input type = 'email' placeholder = 'Email address'  />
+            <label htmlFor = 'email'>Email<sup>*</sup></label><br />
+            <input type = 'email' placeholder = 'Enter your email address' name ='email' onChange = {handleInputChange} />
           </center>
           <center>
-            <input type = 'password' placeholder = 'Password' />
+            <label htmlFor = 'password'>Password<sup>*</sup></label><br />
+            <input type = 'password' placeholder = 'Characters followed by special char' name ='password' onChange = {handleInputChange} />
           </center>
           <center>
-            <button type = 'submit' className = 'login-login' >Login</button>
-          </center>
-          <center>
-            <div className = 'side-info'>
-              <h5>Forgotten password ?</h5>
-              <Link to = '/register' style = {{textDecoration: 'none'}}>
-                <h5 className = 'rtd'>Don't have an account? Sign-up</h5>
-              </Link>
-            </div>
+            <button type = 'submit' className = 'login-login' onClick = {WelcomePage}>Login</button>
           </center>
         </form>
       </div>
-    </div>
-    </div>
+     </div>
+    </div> 
   );
-  }
+ }
 }
 
 export default Student;
